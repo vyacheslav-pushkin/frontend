2a4e8018950d3611dcf41049053b073b48ae2cfe
Vyacheslav Pushkin <v.pushkin@haulmont.com>
2020-03-19 13:17:27 +0400

    WIP: 5

--- scripts/docs/generate-docs-site.js
+++ scripts/docs/generate-docs-site.js
@@ -5,3 +5,5 @@ const childProcess = require('child_process');
 fs.copyFileSync('README.adoc', 'documentation/doc-component-repo/modules/ROOT/pages/index.adoc');
 childProcess.fork(__dirname + '/update-nav.js');
 runCmdSync('npm run antora:gen');
+// create .nojekyll file to allow deploying to GitHub pages:
+fs.closeSync(fs.openSync('documentation/build/site/.nojekyll', 'w'));
